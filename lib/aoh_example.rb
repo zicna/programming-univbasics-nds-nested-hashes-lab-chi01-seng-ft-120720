29b00a63273d1ba5bcd01998e2a6a88094315188
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-22 04:45:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,9 +24,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+  aoh
 end
