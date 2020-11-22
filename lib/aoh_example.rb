790b890ac93be961e182aebe63321a542aea34a9
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-22 04:43:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,12 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, 
+    { name:  "Kate Gompert", occupation: "None" }, 
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
